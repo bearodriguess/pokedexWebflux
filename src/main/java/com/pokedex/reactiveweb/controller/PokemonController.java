bb7b4362e5e21d1d372b2ca9456ef358dcbfb9a4
Bia <beatrizrodrigues19.br@gmail.com>
2020-07-13 16:41:29 -0300

    acesso por localhost:8080 funcionando
    Signed-off-by: Bia <beatrizrodrigues19.br@gmail.com>

---
+++ src/main/java/com/pokedex/reactiveweb/controller/PokemonController.java
@@ -0,0 +1,99 @@
+package com.pokedex.reactiveweb.controller;
+
+import java.awt.PageAttributes.MediaType;
+import java.time.Duration;
+
+import org.springframework.http.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pokedex.reactiveweb.model.Pokemon;
+import com.pokedex.reactiveweb.model.PokemonEvent;
+import com.pokedex.reactiveweb.repository.PokemonRepository;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/pokemons")
+public class PokemonController {
+	
+	private PokemonRepository repository;
+	
+	public PokemonController(PokemonRepository repository) {this.repository = repository;}
+	
+	@GetMapping
+    public Flux<Pokemon> getAllPokemons() {return repository.findAll();}
+	
+	@GetMapping("/{id}")
+    public Mono<ResponseEntity<Pokemon>> getPokemon(@PathVariable String id) {
+        return repository.findById(id)
+                .map(pokemon -> ResponseEntity.ok(pokemon))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+	
+	@PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Pokemon> savePokemon(@RequestBody Pokemon pokemon) {
+        return repository.save(pokemon);
+    }
+	
+	@PutMapping("{id}")
+    public Mono<ResponseEntity<Pokemon>> updatePokemon(@PathVariable(value = "id") String id,
+                                                       @RequestBody Pokemon pokemon) {
+        return repository.findById(id)
+                .flatMap(existingPokemon -> {
+                    existingPokemon.setNome(pokemon.getNome());
+                    existingPokemon.setCategoria(pokemon.getCategoria());
+                    existingPokemon.setHabilidades(pokemon.getHabilidades());
+                    existingPokemon.setPeso(pokemon.getPeso());
+                    return repository.save(existingPokemon);
+                })
+                .map(updatePokemon -> ResponseEntity.ok(updatePokemon))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+
+    }
+	
+	@DeleteMapping("{id}")
+    public Mono<ResponseEntity<Void>> deletePokemon(@PathVariable(value = "id") String id) {
+        return repository.findById(id)
+                .flatMap(existingPokemon ->
+                        repository.delete(existingPokemon)
+                                .then(Mono.just(ResponseEntity.ok().<Void>build()))
+                )
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+	
+	@DeleteMapping
+    public Mono<Void> deleteAllPokemons() {
+        return repository.deleteAll();
+    }
+	
+	 @GetMapping(value = "/events", produces = "text/event-stream")
+	    public Flux<PokemonEvent> getPokemonEvents() {
+	        return Flux.interval(Duration.ofSeconds(5))
+	                .map(val ->
+	                        new PokemonEvent(val, "Product Event")
+	                );
+	    }
+	
+	
+	public PokemonRepository getRepository() {
+		return repository;
+	}
+
+	public void setRepository(PokemonRepository repository) {
+		this.repository = repository;
+	}
+	
+	
+
+}
